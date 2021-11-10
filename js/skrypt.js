8e814822f0675db0412b21303e9d63c159160b7e
Paweł Owsianko <p.owsianko04@gmail.com>
2021-11-10 16:59:07 +0100

    pełny szkielet strony pełny css połowicznie działajacy scrypt

---
+++ js/skrypt.js
@@ -0,0 +1,21 @@
+function rozmiar_planszy(){
+    var field_size = document.getElementById("rozmiar").value
+    document.getElementById("field").appendChild(createTable())  
+} 
+
+function createTable() {
+    var table, row, td, i, j;
+    table = document.createElement('table');
+    
+    for (i=0; i<components.num_of_rows; i++) {
+        row = document.createElement('tr');
+        for (j=0; j<components.num_of_cols; j++) {
+            td = document.createElement('td');
+            td.id = cellID(i, j);
+            row.appendChild(td);
+            addCellListeners(td, i, j);
+        }
+        table.appendChild(row);
+    }
+    return table;
+}
