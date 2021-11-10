e18d2b0af1a9651d350941a02ff939bae9773ab4
Paweł Owsianko <p.owsianko04@gmail.com>
2021-11-10 18:31:02 +0100

    upload niedziałajacego 1 sposobu

--- js/skrypt.js
+++ js/skrypt.js
@@ -1,21 +1,15 @@
-function rozmiar_planszy(){
-    var field_size = document.getElementById("rozmiar").value
-    document.getElementById("field").appendChild(createTable())  
-} 
-
-function createTable() {
-    var table, row, td, i, j;
-    table = document.createElement('table');
-    
-    for (i=0; i<components.num_of_rows; i++) {
-        row = document.createElement('tr');
-        for (j=0; j<components.num_of_cols; j++) {
-            td = document.createElement('td');
-            td.id = cellID(i, j);
-            row.appendChild(td);
-            addCellListeners(td, i, j);
-        }
-        table.appendChild(row);
-    }
-    return table;
+function przewiniecie(){
+    window.scrollTo(1000, 0)
 }
+
+function rozmiar_planszy(){
+    var field_size = document.getElementById("rozmiar").value; 
+    var field = document.querySelector(".field")
+    var divy = document.createElement("div")
+    field.append(divy)
+    if(field.append(divy)){
+        do(field.append(divy)*field_size)
+    }
+}
+
+
