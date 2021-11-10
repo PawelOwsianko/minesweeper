9355c85f94debb886cf43a7f679d2a28145db0da
Paweł Owsianko <p.owsianko04@gmail.com>
2021-11-10 21:57:00 +0100

    połowa js za mna

--- js/skrypt.js
+++ js/skrypt.js
@@ -1,15 +1,34 @@
-function przewiniecie(){
-    window.scrollTo(1000, 0)
-}
-
+var wymiar_planszy;
+var plansza;
+var ikona ='<img src="img/logo.png"/>'
+var sekundy;
+var czasuplywa;
+var czasomierz;
+var funkcje;
 function rozmiar_planszy(){
-    var field_size = document.getElementById("rozmiar").value; 
+    var wymiar_planszy = document.getElementById("rozmiar").value; 
     var field = document.querySelector(".field")
     var divy = document.createElement("div")
     field.append(divy)
-    if(field.append(divy)){
-        do(field.append(divy)*field_size)
+}
+
+function budowa_planszy(wymiar_planszy){
+    for(var i =0; i<wymiar_planszy * wymiar_planszy; i++){
+        plansza.push
     }
 }
+
+function losowa(wymiar_planszy){
+    var losowa = math.floor(math.random()*wymiar_planszy * wymiar_planszy);
+    if(plansza[losowa] === "B"){
+        while(plansza[losowa] === "B"){
+            losowa = math.floor(math.random()* wymiar_planszy * wymiar_planszy);
+        }
+    }
+    return losowa;
+}
+
+
+
 
 
