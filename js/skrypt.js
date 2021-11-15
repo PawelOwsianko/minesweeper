c5f641a07f1349a37e9bbd4ecd21c689bf5c68b5
Paweł Owsianko <p.owsianko04@gmail.com>
2021-11-15 17:55:38 +0100

    mozliwe rozwiazanie problemu z niedziałajacym rozmiarem pola

--- js/skrypt.js
+++ js/skrypt.js
@@ -1,16 +1,21 @@
-var wymiar_planszy;
+var i = 1;
 var plansza;
 var ikona ='<img src="img/logo.png"/>'
 var sekundy;
 var czasuplywa;
 var czasomierz;
 var funkcje;
-function rozmiar_planszy(){
-    var wymiar_planszy = document.getElementById("rozmiar").value; 
-    var field = document.querySelector(".field")
-    var divy = document.createElement("div")
-    field.append(divy)
-}
+
+var button = document.querySelector(".new-game-button");
+button.addEventListener('click', function(){
+    var wymiar_planszy = document.getElementById("rozmiar").value;
+    while(wymiar_planszy>i) 
+    var field = document.querySelector(".field");
+    var divy = document.createElement("div");
+    field.appendChild(divy)*wymiar_planszy;
+    alert('działa?');
+    i++
+    });
 
 function budowa_planszy(wymiar_planszy){
     for(var i =0; i<wymiar_planszy * wymiar_planszy; i++){
@@ -18,15 +23,6 @@ function budowa_planszy(wymiar_planszy){
     }
 }
 
-function losowa(wymiar_planszy){
-    var losowa = math.floor(math.random()*wymiar_planszy * wymiar_planszy);
-    if(plansza[losowa] === "B"){
-        while(plansza[losowa] === "B"){
-            losowa = math.floor(math.random()* wymiar_planszy * wymiar_planszy);
-        }
-    }
-    return losowa;
-}
 
 
 
