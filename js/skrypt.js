ef92643e21ddfd1922db23f1bb2ac3c5560bc79a
pawelowsianko <p.owsianko04@gmail.com>
2021-12-06 21:38:43 +0100

    pojawianie sie pola i znikanie przyciskow

---
+++ js/skrypt.js
@@ -0,0 +1,10 @@
+var bombs = "💣";
+var flaga = "🚩";
+function pole(){
+    var przyciski = document.querySelector('.poczatek');
+    przyciski.classList.add('unvisable');
+    var plansza = document.querySelector('.field');
+        plansza.classList.add('visable');
+        console.log("plansza jest widoczna");
+    }
+console.log("elko")
