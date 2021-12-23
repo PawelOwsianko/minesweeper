f3f9a678f103a51880ab8e686ee746d4f610d86b
pawelowsianko <p.owsianko04@gmail.com>
2021-12-23 21:27:50 +0100

    połowa projektu za mna

--- js/skrypt.js
+++ js/skrypt.js
@@ -1,11 +1,39 @@
-var bombs = "💣";
-var flaga = "🚩";
-//pojawianie sie planszy oraz znikanie przycisku start
-function pole(){
-    var przyciski = document.querySelector('.poczatek');
-    przyciski.classList.add('unvisable');
-    var plansza = document.querySelector('.field');
-        plansza.classList.add('visable');
-        console.log("plansza jest widoczna");
+document.addEventListener('DOMContentLoaded', () => {
+const field = document.querySelector('.field')
+let width = 10;
+let bombAmount = 20;
+let squares = [];
+
+function createBoard () {
+//create arrays
+const bombArray = Array(bombAmount).fill('bomb');
+const emptyArray = Array(width*width - bombAmount).fill('valid');
+const gameArray = emptyArray.concat(bombArray);
+//shuffle it
+const shuffledArray = gameArray.sort(() => Math.random() -0.5);
+//create gameboard
+for (let i = 0; i<width*width; i++) {
+    const square = document.createElement('div')
+    square.setAttribute('id', i)
+    square.classList.add(shuffledArray[i])
+    field.appendChild(square)
+    squares.push(square)
     }
-console.log("elko")
+//adding numbers to 'valid'
+for (let i = 0; i < squares.length; i++) {
+    let total = 0
+   const isLeftEdge = (i % width === 0)
+   const isRightEdge = (i % width === width -1)
+
+   if (squares[i].classList.contains('valid')) {
+        if (i > 0 && !isLeftEdge && squares[i -1].classList.contains('bomb')) total ++
+        if (i > 9 && !isRightEdge && squares[i +1 -width].classList.contains('bomb')) total ++ 
+        squares[i].setAttribute('data', total)
+        console.log(squares[i])
+        }   
+   }
+}
+
+createBoard()
+
+})
